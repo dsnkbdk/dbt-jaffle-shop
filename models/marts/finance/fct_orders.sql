66263c5c49dec10dbfb70d6269b6c60578bd5b34
Wennan Shi <wennanshi@Wennans-MacBook-Pro.local>
2026-02-10 19:22:53 +1300

    Added incremental model

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,12 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'order_id',
+        incremental_strategy = 'merge'
+    )
+}}
+
+
 with orders as (
     select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
@@ -27,3 +36,9 @@ final as (
 )
 
 select * from final
+
+{% if is_incremental() %}
+    where order_date >= (select max(order_date) from {{ this }}) 
+{% endif %}
+
+order by order_date desc
