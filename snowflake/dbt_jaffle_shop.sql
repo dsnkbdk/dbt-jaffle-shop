5df85b496e137b1ebd5aea7b3ebeddb667af6faa
Wennan Shi <wennanshi@Wennans-MacBook-Pro.local>
2026-02-16 15:52:39 +1300

    Update snowflake sql

--- snowflake/dbt_jaffle_shop.sql
+++ snowflake/dbt_jaffle_shop.sql
@@ -1,25 +1,33 @@
-create or replace warehouse transforming;
-create or replace database analytics;
-create or replace database raw;
-create or replace schema raw.jaffle_shop;
-create or replace schema raw.stripe;
+---> set Role
+USE ROLE accountadmin;
+---> set Warehouse
+USE WAREHOUSE compute_wh;
 
-create table raw.jaffle_shop.customers 
-( id integer,
+CREATE OR REPLACE DATABASE raw;
+CREATE OR REPLACE DATABASE analytics;
+CREATE OR REPLACE SCHEMA raw.stripe;
+CREATE OR REPLACE SCHEMA raw.jaffle_shop;
+
+
+CREATE OR REPLACE TABLE raw.jaffle_shop.customers
+(
+id integer,
 first_name varchar,
 last_name varchar
 );
 
-create table raw.jaffle_shop.orders
-( id integer,
+CREATE OR REPLACE TABLE raw.jaffle_shop.orders
+(
+id integer,
 user_id integer,
 order_date date,
 status varchar,
 _etl_loaded_at timestamp_ltz default current_timestamp()
 );
 
-create table raw.stripe.payments 
-( id integer,
+CREATE OR REPLACE TABLE raw.stripe.payments
+(
+id integer,
 orderid integer,
 paymentmethod varchar,
 status varchar,
@@ -28,48 +36,87 @@ created date,
 _batched_at timestamp_ltz default current_timestamp()
 );
 
-create or replace stage raw.public.stg_dbt_tutorial
-url = 'azure://wennandbt.blob.core.windows.net/aws-s3-demo-wennan/dbt-tutorial/'
-credentials = (azure_sas_token = 'sp=rl&st=2026-02-08T04:15:57Z&se=2026-02-13T12:30:57Z&spr=https&sv=2024-11-04&sr=c&sig=3sw914wIOrSVG3IqnaHt68DsY3j2%2BotYR1MTApds0YE%3D')
+CREATE OR REPLACE STAGE raw.public.loading
+URL = 'azure://wennandbt.blob.core.windows.net/jaffle-shop/'
+credentials = (azure_sas_token = 'sp=rl&st=2026-02-16T02:07:06Z&se=2026-03-16T10:22:06Z&spr=https&sv=2024-11-04&sr=c&sig=YpNC0GgbLIIphIuQaxkyYOAmg4fpy1xRZr5qgISGlf8%3D')
 file_format = (
-type = 'CSV'
+type = CSV
 field_delimiter = ','
 skip_header = 1
 );
 
-copy into raw.jaffle_shop.customers (id, first_name, last_name)
-from @raw.public.stg_dbt_tutorial
+
+COPY INTO raw.jaffle_shop.customers
+(
+id,
+first_name,
+last_name
+)
+from @raw.public.loading/customers/
 files = ('jaffle_shop_customers.csv');
 
-copy into raw.jaffle_shop.orders (id, user_id, order_date, status)
-from @raw.public.stg_dbt_tutorial
+COPY INTO raw.jaffle_shop.orders
+(
+id,
+user_id,
+order_date,
+status
+)
+from @raw.public.loading/orders/
 files = ('jaffle_shop_orders.csv');
 
-copy into raw.stripe.payments (id, orderid, paymentmethod, status, amount, created)
-from @raw.public.stg_dbt_tutorial
+COPY INTO raw.stripe.payments
+(
+id,
+orderid,
+paymentmethod,
+status,
+amount,
+created
+)
+from @raw.public.loading/payments/
 files = ('stripe_payments.csv');
 
 
-insert into raw.jaffle_shop.orders (id, user_id, order_date, status)
+INSERT INTO raw.jaffle_shop.orders
+(
+id,
+user_id,
+order_date,
+status
+)
 values
-  (100, 100, '2025-02-15', 'shipped'),
-  (101, 84,  '2025-02-15', 'shipped'),
-  (102, 42,  '2025-02-15', 'shipped'),
-  (103, 101, '2025-02-15', 'shipped'),
-  (104, 66,  '2025-02-15', 'shipped');
+(100, 100, '2025-02-15', 'shipped'),
+(101, 84,  '2025-02-15', 'shipped'),
+(102, 42,  '2025-02-15', 'shipped'),
+(103, 101, '2025-02-15', 'shipped'),
+(104, 66,  '2025-02-15', 'shipped');
 
-insert into raw.jaffle_shop.customers (id, first_name, last_name)
+
+INSERT INTO raw.jaffle_shop.customers
+(
+id,
+first_name,
+last_name
+)
 values
-  (101, 'Michelle', 'B.'),
-  (102, 'Faith',    'L.');
+(101, 'Michelle', 'B.'),
+(102, 'Faith',    'L.');
 
-insert into raw.stripe.payments (id, orderid, paymentmethod, status, amount, created)
+
+INSERT INTO raw.stripe.payments
+(
+id,
+orderid,
+paymentmethod,
+status,
+amount,
+created
+)
 values
-  (121, 100, 'bank_transfer', 'success', 1000, '2025-02-14'),
-  (122, 101, 'credit_card',   'fail',     400, '2025-02-14'),
-  (123, 102, 'credit_card',   'success', 1900, '2025-02-14'),
-  (124, 103, 'credit_card',   'success', 1000, '2025-02-15'),
-  (125, 104, 'coupon',        'success',  100, '2025-02-15');
-
-
+(121, 100, 'bank_transfer', 'success', 1000, '2025-02-14'),
+(122, 101, 'credit_card',   'fail',     400, '2025-02-14'),
+(123, 102, 'credit_card',   'success', 1900, '2025-02-14'),
+(124, 103, 'credit_card',   'success', 1000, '2025-02-15'),
+(125, 104, 'coupon',        'success',  100, '2025-02-15');
 
